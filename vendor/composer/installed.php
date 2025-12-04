2801efd692e7fd9415adbce7fa81304d4693b0bf
Alfredo Navas <elpuas@gmail.com>
2025-12-04 16:56:46 -0600

    - MVP

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f6c1907ed37eca6ca28d5f339dad7313dd7c6909',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => true,
+        'dev' => false,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f6c1907ed37eca6ca28d5f339dad7313dd7c6909',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
